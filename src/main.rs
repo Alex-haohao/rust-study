7ef3a6ba8ca06347683eb99dd0eccc0969261f70
Tianhao Xi <xth6902716@gmail.com>
2021-10-31 19:16:42 +0800

    update chapter7

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,2 @@
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter,
-}
-
-fn value_in_coin(coin: Coin) -> u32 {
-    match coin {
-        Coin::Penny => 1,
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter => 25,
-    }
-}
-
+// packag,create,module
 fn main() {}
