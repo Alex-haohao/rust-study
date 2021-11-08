a209983494ae335a8459a611d3ad1d81fc8ff1ea
Tianhao Xi <xth6902716@gmail.com>
2021-11-08 23:30:30 +0800

    create a simple command line grep

---
+++ src/lib.rs
@@ -0,0 +1,53 @@
+use std::error::Error;
+use std::fs;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let content = fs::read_to_string(config.filename)?;
+    for line in search(&config.query, &content) {
+        println!("{}", line);
+    }
+    Ok(())
+}
+
+pub struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() != 3 {
+            return Err("This program takes two arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
+pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in content.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+pick three";
+        assert_eq!(vec!["safe", "fast", "productive"], search(query, contents));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,16 @@
-// packag,create,module
-fn main() {}
+use rust_study::Config;
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        std::process::exit(1);
+    });
+
+    if let Err(e) = rust_study::run(config) {
+        println!("Application error: {}", e);
+        std::process::exit(1);
+    }
+}
